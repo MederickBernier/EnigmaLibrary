45fcca65d3d86f079ff622504e3fcd75d58784b5
Mederick Bernier <mederick.bernier@hotmail.ca>
2024-08-07 16:21:12 -0400

    added Logging

---
+++ LoggingUtils.cs
@@ -0,0 +1,20 @@
+﻿namespace EnigmaLibrary;
+public static class LoggingUtils {
+    private static readonly string logFilePath = "log.txt";
+
+    public static void LogError(string message) {
+        Log("ERROR", message);
+    }
+
+    public static void LogInfo(string message) {
+        Log("INFO", message);
+    }
+
+    public static void LogWarning(string message) {
+        Log("WARNING", message);
+    }
+
+    private static void Log(string logType, string message) {
+        File.AppendAllText(logFilePath, $"{logType}: {DateTime.Now}: {message}{Environment.NewLine}");
+    }
+}
