bbc0c6b238c87990d186f6a59bf66ef494984811
Mederick Bernier <mederick.bernier@hotmail.ca>
2024-08-07 15:43:03 -0400

    added credit card checksum validation

---
+++ CreditCardUtils.cs
@@ -0,0 +1,16 @@
+﻿namespace EnigmaLibrary;
+public static class CreditCardUtils {
+    public static bool IsValidCreditCardNumber(string cardNumber) {
+        if (string.IsNullOrWhiteSpace(cardNumber)) return false;
+
+        int[] cardDigits = cardNumber.Where(char.IsDigit).Select(c => int.Parse(c.ToString())).ToArray();
+        int checksum = 0;
+
+        for (int i = cardDigits.Length - 1; i >= 0; i -= 2) {
+            int doubledValue = cardDigits[i] * 2;
+            checksum += (doubledValue > 9 ? doubledValue - 9 : doubledValue) + (i > 0 ? cardDigits[i - 1] : 0);
+        }
+
+        return checksum % 10 == 0;
+    }
+}
