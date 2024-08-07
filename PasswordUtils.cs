651ce26b075515b60b17393159dbd87da46fb551
Mederick Bernier <mederick.bernier@hotmail.ca>
2024-08-07 15:41:59 -0400

    added password utils

---
+++ PasswordUtils.cs
@@ -0,0 +1,13 @@
+﻿namespace EnigmaLibrary;
+public static class PasswordUtils {
+    public static bool IsStrongPassword(string password) {
+        if (string.IsNullOrWhiteSpace(password)) return false;
+
+        bool hasUpperCase = password.Any(char.IsUpper);
+        bool hasLowerCase = password.Any(char.IsLower);
+        bool hasNumbers = password.Any(char.IsDigit);
+        bool hasSpecialChar = password.Any(ch => !char.IsLetterOrDigit(ch));
+
+        return password.Length >= 8 && hasUpperCase && hasLowerCase && hasNumbers && hasSpecialChar;
+    }
+}
