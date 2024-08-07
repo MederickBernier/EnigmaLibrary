c6899e8fda216db91f18a75dcbb2513d5cdd0291
Mederick Bernier <mederick.bernier@hotmail.ca>
2024-08-07 15:51:54 -0400

    added http utils

---
+++ HttpUtils.cs
@@ -0,0 +1,17 @@
+﻿namespace EnigmaLibrary;
+public static class HttpUtils {
+    private static readonly HttpClient _client = new HttpClient();
+
+    public static async Task<string> GetHttpResponse(string url) {
+        HttpResponseMessage response = await _client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
+    }
+
+    public static async Task<string> PostHttpRequest(string url, string postData) {
+        HttpContent content = new StringContent(postData);
+        HttpResponseMessage response = await _client.PostAsync(url, content);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
+    }
+}
