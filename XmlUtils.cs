8c7af1289d0e3f42f24c150aca61d3900d4a525f
Mederick Bernier <mederick.bernier@hotmail.ca>
2024-08-07 16:11:07 -0400

    added xml utils.

---
+++ XmlUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Serialization;
+
+namespace EnigmaLibrary;
+public static class XmlUtils {
+    public static string SerializeToXml<T>(T obj) {
+        XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+        using (StringWriter sw = new StringWriter()) {
+            xmlSerializer.Serialize(sw, obj);
+            return sw.ToString();
+        }
+    }
+
+    public static T DeserializeFromXml<T>(string xml) {
+        XmlSerializer serializer = new XmlSerializer(typeof(T));
+        using (StringReader sr = new StringReader(xml)) {
+            return (T)serializer.Deserialize(sr);
+        }
+    }
+}
