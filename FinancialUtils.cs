1e4b56c35717d9efe810e218355473455b4e9722
Mederick Bernier <mederick.bernier@hotmail.ca>
2024-08-07 15:46:18 -0400

    added maths and financial

---
+++ FinancialUtils.cs
@@ -0,0 +1,11 @@
+﻿namespace EnigmaLibrary;
+public static class FinancialUtils {
+    public static decimal CalculateCompoundInterest(decimal principal, double rate, int timesCompounded, int years) {
+        double compoundFactor = Math.Pow((1 + rate / timesCompounded), timesCompounded * years);
+        return principal * (decimal)compoundFactor;
+    }
+
+    public static decimal ConvertCurrency(decimal amount, decimal conversionRate) {
+        return amount * conversionRate;
+    }
+}
---
+++ MathUtils.cs
@@ -0,0 +1,20 @@
+﻿namespace EnigmaLibrary;
+public static class MathUtils {
+    public static double CalculatePercentage(double total, double value) {
+        if (total == 0) throw new DivideByZeroException("total cannot be zero");
+        return (value / total) * 100;
+    }
+
+    public static int FindGreatestCommonDivisor(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static int FindLeastCommonMultiple(int a, int b) {
+        return (a / FindGreatestCommonDivisor(a, b) * b);
+    }
+}
