dd52e3f90555a3d32d681a69bf123f933cfac0eb
Mederick Bernier <mederick.bernier@hotmail.ca>
2024-08-07 16:05:55 -0400

    added json support

---
+++ JsonUtils.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace EnigmaLibrary;
+public static class JsonUtils {
+    public static string SerializeToJson<T>(T obj) {
+        return JsonSerializer.Serialize(obj);
+    }
+
+    public static T DeserializeFromJson<T>(string json) {
+        return JsonSerializer.Deserialize<T>(json);
+    }
+}
