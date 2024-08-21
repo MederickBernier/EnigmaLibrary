6a41946cf62e9825f7d696fb37e57c2940217399
Mederick Bernier <mederick.bernier@hotmail.ca>
2024-08-21 09:39:45 -0400

    added some more functions to http

--- src/Http/HttpUtils.php
+++ src/Http/HttpUtils.php
@@ -2,7 +2,8 @@
 
 namespace EnigmaLibrary\Http;
 
-class HttpUtils{
+class HttpUtils
+{
     /**
      * Sends an HTTP GET request.
      * 
@@ -12,7 +13,8 @@ class HttpUtils{
      * @throws \Exception if the request fails.
      */
 
-     public static function sendGetRequest(string $url, array $headers = []):string{
+    public static function sendGetRequest(string $url, array $headers = []): string
+    {
         $options = [
             'http' => [
                 'header' => $headers,
@@ -22,48 +24,50 @@ class HttpUtils{
 
         $context = stream_context_create($options);
         $response = file_get_contents($url, false, $context);
-        if($response === false){
+        if ($response === false) {
             throw new \Exception("GET request to {$url} failed.");
         }
         return $response;
-     }
+    }
 
-     /**
-      * Sends an HTTP POST request.
-      *
-      * @param string $url The URL to send the request to.
-      * @param array $data The data to include in the POST body.
-      * @param array $headers Optional headers to include in the request.
-      * @return string The response content.
-      * @throws \Exception if the request fails.
-      */
-      public static function sendPostRequest(string $url, array $data, array $headers = []):string{
-          $options = [
+    /**
+     * Sends an HTTP POST request.
+     *
+     * @param string $url The URL to send the request to.
+     * @param array $data The data to include in the POST body.
+     * @param array $headers Optional headers to include in the request.
+     * @return string The response content.
+     * @throws \Exception if the request fails.
+     */
+    public static function sendPostRequest(string $url, array $data, array $headers = []): string
+    {
+        $options = [
             'http' => [
                 'header' => array_merge(['Content-type: application/x-www-form-urlencoded'], $headers),
                 'method' => 'POST',
                 'content' => http_build_query($data),
             ]
-          ];
-          $context = stream_context_create($options);
-          $response = file_get_contents($url, false, $context);
+        ];
+        $context = stream_context_create($options);
+        $response = file_get_contents($url, false, $context);
 
-          if($response === false){
+        if ($response === false) {
             throw new \Exception("POST request to {$url} failed.");
-          }
-          return $response;
-      }
+        }
+        return $response;
+    }
 
-      /**
-       * Formats a response in the specified format (JSON, XML, etc.)
-       * 
-       * @param mixed $data The data to format.
-       * @param string $format The format to use (default is 'json')
-       * @return string The formatted response.
-       * @throws \Exception if the format is not supported.
-       */
-      public static function formatResponse($data, string $format = 'json'):string{
-        switch($format){
+    /**
+     * Formats a response in the specified format (JSON, XML, etc.)
+     * 
+     * @param mixed $data The data to format.
+     * @param string $format The format to use (default is 'json')
+     * @return string The formatted response.
+     * @throws \Exception if the format is not supported.
+     */
+    public static function formatResponse($data, string $format = 'json'): string
+    {
+        switch ($format) {
             case 'json':
                 header('Content-Type: application/json');
                 return json_encode($data);
@@ -73,39 +77,83 @@ class HttpUtils{
             default:
                 throw new \Exception("Format {$format} not supported.");
         }
-      }
+    }
 
-      /**
-       * Converts an array to XML format
-       * 
-       * @param mixed $data The data to convert.
-       * @param string $rootNodeName The root node name for the XML (default is 'data').
-       * @param \SimpleXMLElement|null $xml The SimpleXMLElement object for recursion.
-       * @return string the XML formatted string.
-       */
-      public static function toXml($data, $rootNodeName = 'data', $xml = null):string{
-        if($xml === null){
+    /**
+     * Converts an array to XML format
+     * 
+     * @param mixed $data The data to convert.
+     * @param string $rootNodeName The root node name for the XML (default is 'data').
+     * @param \SimpleXMLElement|null $xml The SimpleXMLElement object for recursion.
+     * @return string the XML formatted string.
+     */
+    public static function toXml($data, $rootNodeName = 'data', $xml = null): string
+    {
+        if ($xml === null) {
             $xml = new \SimpleXMLElement("<{$rootNodeName}/>");
         }
-        foreach($data as $key => $value){
-            if(is_array($value)){
+        foreach ($data as $key => $value) {
+            if (is_array($value)) {
                 self::toXml($value, $key, $xml->addChild($key));
-            }else{
+            } else {
                 $xml->addChild($key, htmlspecialchars($value));
             }
         }
         return $xml->asXml();
-      }
+    }
 
-      /**
-       * Handles HTTP redirects.
-       * 
-       * @param string $url The url to redirect to.
-       * @param int $statusCode The HTTP status code for the redirect (default is 302)
-       * @return void
-       */
-      public static function redirect(string $url, int $statusCode = 302):void{
-        header("Location:{$url}",true, $statusCode);
+    /**
+     * Handles HTTP redirects.
+     * 
+     * @param string $url The url to redirect to.
+     * @param int $statusCode The HTTP status code for the redirect (default is 302)
+     * @return void
+     */
+    public static function redirect(string $url, int $statusCode = 302): void
+    {
+        header("Location:{$url}", true, $statusCode);
         exit();
-      }
+    }
+
+    public static function sendPutRequest(string $url, array $data, array $headers = []): string
+    {
+        $options = [
+            'http' => [
+                'header'  => array_merge(['Content-type: application/x-www-form-urlencoded'], $headers),
+                'method'  => 'PUT',
+                'content' => http_build_query($data),
+            ]
+        ];
+        $context = stream_context_create($options);
+        $response = file_get_contents($url, false, $context);
+
+        if ($response === false) {
+            throw new \Exception("PUT request to {$url} failed.");
+        }
+
+        return $response;
+    }
+
+    public static function sendDeleteRequest(string $url, array $headers = []): string
+    {
+        $options = [
+            'http' => [
+                'header' => $headers,
+                'method' => 'DELETE',
+            ]
+        ];
+        $context = stream_context_create($options);
+        $response = file_get_contents($url, false, $context);
+
+        if ($response === false) {
+            throw new \Exception("DELETE request to {$url} failed.");
+        }
+
+        return $response;
+    }
+
+    public static function buildQueryString(array $params): string
+    {
+        return http_build_query($params);
+    }
 }
