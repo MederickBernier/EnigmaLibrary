282096d6465d071c5aa89c4a03b1ba7f9931a8ce
Mederick Bernier <mederick.bernier@hotmail.ca>
2024-08-21 07:49:21 -0400

    removed the interfaces because i'll be using static functions instead

--- src/Array/DefaultArrayUtils.php
+++ src/Array/DefaultArrayUtils.php
@@ -2,12 +2,9 @@
 
 namespace EnigmaLibrary\Array;
 
-class DefaultArrayUtils implements ArrayUtilsInterface{
+class DefaultArrayUtils{
 
-    /**
-     * {@inheritDoc}
-     */
-    public function flatten(array $array): array {
+    public static function flatten(array $array): array {
         $result = [];
         array_walk_recursive($array, function($a) use (&$result){
             $result[] = $a;
@@ -15,24 +12,18 @@ class DefaultArrayUtils implements ArrayUtilsInterface{
         return $result;
      }
 
-    /**
-     * {@inheritDoc}
-     */
-    public function pluck(array $array, string $key): array {
+    public static function pluck(array $array, string $key): array {
         return array_map(function($v) use($key){
             return $v[$key] ?? null;
         },$array);
      }
 
-    /**
-     * {@inheritDoc}
-     */
-    public function mergeRecursiveDistinct(array $array1, array $array2): array {
+    public static function mergeRecursiveDistinct(array $array1, array $array2): array {
         $merged = $array1;
 
         foreach($array2 as $key => $value){
             if(is_array($value) && isset($merged[$key]) && is_array($merged[$key])){
-                $merged[$key] = $this->mergeRecursiveDistinct($merged[$key],$value);
+                $merged[$key] = self::mergeRecursiveDistinct($merged[$key],$value);
             }else{
                 $merged[$key] = $value;
             }
---
+++ src/Database/DefaultDbUtils.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace EnigmaLibrary\Database;
+
+use PDO;
+
+class DefaultDbUtils{
+    /**
+     * Connects to a database using PDO
+     * 
+     * @param string $dnb The Data Source Name (DSN) for the database connection.
+     * @param string $username The username for the database connection.
+     * @param string $password The password for the database connection.
+     * @return \PDO The PDO instance for the database connection.
+     */
+    public static function connectToDatabase(string $dsn, string $username, string $password):PDO{
+        try{
+            $pdo = new \PDO($dsn, $username, $password);
+            $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        }catch(\PDOException $e){
+            throw new \Exception('Database connection failed: '.$e->getMessage());
+        }
+    }
+
+    /**
+     * Builds a SELECT SQL query
+     * 
+     * @param string $table The name of the table
+     * @param array $conditions An associatve array of conditions for the WHERE clause
+     * @param array $columns An array of columns to select. Defaults to all columns
+     * @return string the SQL query string
+     */
+    public static function buildSelectQuery(string $table, array $conditions = [], array $columns = ['*']):string{
+        $columnsString = implode(',',$columns);
+        $query = "SELECT {$columnsString} FROM {$table}";
+
+        if(!empty($conditions)){
+            $whereClauses = array_map(
+                fn($key, $value) => sprintf("%s = '%s'", $key, addslashes($value)),
+                array_keys($conditions),
+                $conditions
+            );
+            $query .= ' WHERE '.implode(' AND ', $whereClauses);
+        }
+        return $query;
+    }
+}
